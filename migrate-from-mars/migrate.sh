c4f5673938d320cd05936f49ac5ea8f939c24dab
Trey Mosley <iiimosley@gmail.com>
2024-05-15 09:06:05 -0500

    Implement ampersand replacement for directory names

--- migrate-from-mars/migrate.sh
+++ migrate-from-mars/migrate.sh
@@ -29,11 +29,6 @@ find . -type d -path '*/WAV/*Kits' | while read -r dir; do
     mkdir -p "$sample_dir"
   fi
 
-  ### TODO:
-  # - Remove Parent Directory string from new_dir 
-  # - Remove any string from kit name from wave file names
-  #   - remove double digit numbers
-  #   - number any duplicate names 
   find "$dir" -type d -exec sh -c '(ls -p "{}"|grep />/dev/null)||echo "{}"' \; | while read -r kit_dir; do
     # Model a new directory name from sub path
     new_dir=`echo "$kit_dir" | awk '{
@@ -41,6 +36,7 @@ find . -type d -path '*/WAV/*Kits' | while read -r dir; do
       print
     }' |                       # <-- Remove path prefix
     tr '/' ' ' |               # <-- Replace slashes with spaces
+    tr '&' '+' |               # <-- Replace ampersands with pluses (for XML path reads)
     sed -E 's/[0-9]{2}\.//g' | # <-- Remove any leading numbers
     tr -s ' ' |                # <-- Remove extra spaces
     awk '{$1=$1};1'`           # <-- Trim leading/trailing whitespace
@@ -49,6 +45,10 @@ find . -type d -path '*/WAV/*Kits' | while read -r dir; do
     echo "$kit_dir/* --> $deluge_kit_dir/"
     
     if [ "$is_test" != "1" ]; then
+      ## TODO
+      # - Remove trailing digits at end of wav file name
+      # - Number any duplicate wav names (post-trailing digits removal)  
+
       # Copy the kit directory content to the new destination
       mkdir -p "$deluge_kit_dir"
       cp -R "$kit_dir"/*.wav "$deluge_kit_dir/"
