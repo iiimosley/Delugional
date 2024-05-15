82e1e956deb75954cf92c31741b9ae730df466d5
Trey Mosley <iiimosley@gmail.com>
2024-05-15 09:30:41 -0500

    Remove unused vars and update comments

--- migrate-from-mars/construct.sh
+++ migrate-from-mars/construct.sh
@@ -6,9 +6,6 @@ source ./shared/_setup.sh "$@"
 sample_dir="$DELUGE_SD_DIR/SAMPLES/SfM"
 kits_dir="$DELUGE_SD_DIR/KITS"
 
-kit_temp="./templates/kit/base.xml"
-sound_temop="./templates/kit/sound.xml"
-
 find "$sample_dir" -mindepth 2 -maxdepth 2 -type d | while read -r dir; do
     parent_dir=$(dirname "$dir" | xargs basename)
     child_dir=$(basename "$dir")
@@ -44,11 +41,11 @@ find "$sample_dir" -mindepth 2 -maxdepth 2 -type d | while read -r dir; do
         sound_xml_block=${sound_xml_block//SAMPLE_PATH/$local_sample_path}
         sound_xml_block=${sound_xml_block//SAMPLE_END_MS/$end_ms}
 
-        #### RUFF STUFF - not a fun package. Reverting to dumb soluton.
+        # #### RUFF STUFF - xml(starlet) is not a fun package. Reverting to dumb soluton.
         # # Add the sound_xml_block to the <soundSources> in the $kit_file
         # xml esc "$kit_file"
-        # xmlstarlet ed -L -a '/kit/soundSources' -t elem -n sound -v "$sound_xml_block" "$kit_file"
-        # xmlstarlet fo -R -H "$kit_file"
+        # xml ed -L -a '/kit/soundSources' -t elem -n sound -v "$sound_xml_block" "$kit_file"
+        # xml fo -R -H "$kit_file"
 
         echo $sound_xml_block >> "$kit_file"
     done
@@ -56,11 +53,14 @@ find "$sample_dir" -mindepth 2 -maxdepth 2 -type d | while read -r dir; do
     # Close tags on the kit file xml
     cat ./templates/kit/tail.xml.sample >> "$kit_file"
 
-    # Send this on to the Deluge SD card
+    # Format XML & send this on to the Deluge SD card
     xml fo "$kit_file" > "$kits_dir/$kit_name.xml"
 
     ### only create one kit if we are testing ###
     if [ "$is_test" == "1" ]; then
+        echo "Test complete:" 
+        echo "Review migrated Kit XML at $kits_dir/$kit_name.xml"
+        echo "Review source Kit XML at $kit_file"
         break
     fi
 done
